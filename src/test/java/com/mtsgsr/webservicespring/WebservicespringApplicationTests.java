ec3b029fd03127af6182125f1664464c2c0f8201
Matheus Gomes <maatheus.gsr@gmail.com>
2023-09-05 23:52:36 -0300

    feat: add initial setup

---
+++ src/test/java/com/mtsgsr/webservicespring/WebservicespringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtsgsr.webservicespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservicespringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
