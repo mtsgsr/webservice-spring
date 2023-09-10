d981adc84ba02dc974908f0aad0cfc7d9f16fd27
Matheus Gomes <maatheus.gsr@gmail.com>
2023-09-10 15:59:35 -0300

    feat: add user service layer and component registration

---
+++ src/main/java/com/mtsgsr/webservicespring/services/UserService.java
@@ -0,0 +1,27 @@
+package com.mtsgsr.webservicespring.services;
+
+import com.mtsgsr.webservicespring.entities.User;
+import com.mtsgsr.webservicespring.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
