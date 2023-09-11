982e1967f09dfbb04d2ce7711ff43d4db599d67d
Matheus Gomes <maatheus.gsr@gmail.com>
2023-09-11 18:27:23 -0300

    feat: add many to many association with join table between category and product

--- src/main/java/com/mtsgsr/webservicespring/config/TestConfig.java
+++ src/main/java/com/mtsgsr/webservicespring/config/TestConfig.java
@@ -49,6 +49,15 @@ public class TestConfig implements CommandLineRunner {
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
+        p1.getCategories().add(cat2);
+        p2.getCategories().add(cat1);
+        p2.getCategories().add(cat3);
+        p3.getCategories().add(cat3);
+        p4.getCategories().add(cat3);
+        p5.getCategories().add(cat2);
+
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
 
         User u1 = new User(null, "Alex Scott", "alex@gmail.com", "12345678", "123456");
         User u2 = new User(null, "James Williams", "james@gmail.com", "87654321", "123456");
