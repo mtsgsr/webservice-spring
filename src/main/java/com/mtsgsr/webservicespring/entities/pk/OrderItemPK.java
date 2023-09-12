c9791f39f0e5f8659198aa42863f90f8c18e9587
Matheus Gomes <maatheus.gsr@gmail.com>
2023-09-12 14:18:26 -0300

    feat: add order item and order item pk with many to one associations

---
+++ src/main/java/com/mtsgsr/webservicespring/entities/pk/OrderItemPK.java
@@ -0,0 +1,50 @@
+package com.mtsgsr.webservicespring.entities.pk;
+
+import com.mtsgsr.webservicespring.entities.Order;
+import com.mtsgsr.webservicespring.entities.Product;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.util.Objects;
+
+@Embeddable
+public class OrderItemPK {
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        OrderItemPK that = (OrderItemPK) obj;
+        return Objects.equals(order, that.order) && Objects.equals(product, that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(order, product);
+    }
+}
