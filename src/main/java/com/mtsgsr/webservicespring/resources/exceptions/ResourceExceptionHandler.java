4db8e437ae717126409b996ebe072549991b56fe
Matheus Gomes <maatheus.gsr@gmail.com>
2023-09-13 17:33:43 -0300

    feat: add database exception

--- src/main/java/com/mtsgsr/webservicespring/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/mtsgsr/webservicespring/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.mtsgsr.webservicespring.resources.exceptions;
 
+import com.mtsgsr.webservicespring.services.exceptions.DatabaseException;
 import com.mtsgsr.webservicespring.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -16,6 +17,14 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
         String errorMsg = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError error = new StandardError(Instant.now(), status.value(), errorMsg, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(error);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+        String errorMsg = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError error = new StandardError(Instant.now(), status.value(), errorMsg, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(error);
     }
